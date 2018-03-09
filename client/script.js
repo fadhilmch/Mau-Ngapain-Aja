a71ec2573b784b16836b91a79b6bd91ce5a14598
Fadhilmch <fadhil.mochammad1095@gmail.com>
2018-03-09 09:40:01 +0700

    merged

--- client/script.js
+++ client/script.js
@@ -1,1 +1,21 @@
-console.log($('h3'))
+function getList () {
+    $.ajax({
+        url: 'http://localhost:3000/list',
+        method: 'get',
+        dataType: 'JSON',
+        success: (response) => {
+            populateList(response);
+        }
+    })
+}
+
+function populateList(data) {
+    $('#list').empty();
+    data.forEach(list => {
+        $('#list').append(`
+            <li><a href="">${list.title}</a></li>
+        `)
+    });
+}
+
+getList();
