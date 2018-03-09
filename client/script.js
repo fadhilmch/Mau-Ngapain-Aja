dc5ce10309eb56b7945085e89daa64aa64cf7658
Fadhilmch <fadhil.mochammad1095@gmail.com>
2018-03-09 11:33:43 +0700

    updated jquery

--- client/script.js
+++ client/script.js
@@ -11,5 +11,31 @@ axios.get('http://localhost:3000',{
 
   })
   .catch(function (error) {
-    window.location.href = 'login.html'
+    // window.location.href = 'login.html'
   });
+
+function getList() {
+    axios.get('http://localhost:3000/lists')
+        .then((response) => {
+            response.data.data.forEach(list => {
+                $('#list').append(`
+                    <li><a href="#" onclick="getTodo('${list._id}')">${list.title}</a></li>
+                `)
+            })
+        })
+}
+
+function getTodo(idList) {
+    axios.get(`http://localhost:3000/lists/${idList}`)
+        .then(response => {
+            $('#todo').empty();
+            response.data.data.todo.forEach(todo => {
+                $('#todo').append(`
+                    <div class=row><a href="#"><i class="material-icons side_bar_icons_edit">crop_square</i></a>${todo.text}</div>
+                `)
+            })
+            console.log(response.data.data.todo);
+        })
+}
+
+getList();
