720190734c5d866e744adf3cd5d5b5517ec1843f
kevinhimawanhie <kevinhimawanhie@gmail.com>
2018-03-09 08:42:23 +0700

    Done css login and 50% login home

---
+++ client/script.js
@@ -0,0 +1,23 @@
+
+$('#login_switch').click(function(){
+    $('#signup_form').css('display','none')
+    $('#login_form').css('display','block')
+    
+})
+
+$('#signup_switch').click(function(){
+    $('#login_form').css('display','none')
+    $('#signup_form').css('display','block')
+})
+
+$('#login_switch').mousedown(function(){
+    event.preventDefault();
+    console.log('hello')
+})
+
+
+function defaultOff(){
+    
+}
+
+// Axios Client Goes Here
